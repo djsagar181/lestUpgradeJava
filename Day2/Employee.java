158a6255e1ee677e1ab488b66b4c5716d57ee008
djsagar181 <djsagar181@gmail.com>
2020-11-01 20:37:28 +0530

    Day2 assignments

---
+++ Day2/Employee.java
@@ -0,0 +1,15 @@
+package com.sagar;
+
+public class Employee {
+     String name;
+     int age;
+     String city;
+
+     public void disply(){
+         System.out.println("the name is:"+name);
+         System.out.println("the age is:"+age);
+         System.out.println("the city is:"+city);
+     }
+
+
+}
---
+++ Day2/Main.java
@@ -0,0 +1,19 @@
+package com.sagar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee e=new Employee();
+            e.name="SK";
+            e.age=24;
+            e.city="Thane";
+        Employee e1=new Employee();
+            e1.name="Sagar";
+            e1.age=25;
+            e1.city="Mumbai";
+    e.disply();
+    e1.disply();
+    }
+
+
+}
